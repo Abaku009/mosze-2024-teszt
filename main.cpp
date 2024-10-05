21634e9c2a4c6ca967922fdcc5e5afbd10d4de43
Abaku009 <davidszabolcs15@gmail.com>
2024-10-05 15:56:31 +0200

    marked the errors in comments

--- main.cpp
+++ main.cpp
@@ -5,22 +5,22 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    std::cout << '1-100 ertekek duplazasa'   // for strings use "" quotes, and use ; at the end
+    for (int i = 0;)                        // this for loop is not correctly set up to iterate over the array
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)                // this for loop is not correctly set up to iterate over the array
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"             // use ; at the end and there is nothing to write out after "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag;                                          // The variable "atlag" should be initialized to 0 before accumulating values
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i]                   // use ; at the end       
     }
     atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
+    std::cout << "Atlag: " << atlag << std::endl;           // Don't forget to free the allocated memory
     return 0;
 }
